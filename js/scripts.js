653e9dc52551bde61d263323058e1416f8caa2ec
Radássila Silva <134161509+radassila@users.noreply.github.com>
2024-04-17 00:26:38 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -97,3 +97,24 @@ function getLocationAndCity() {
         console.error('Geolocalização não é suportada neste navegador.');
     }
 }
+
+function renderizarMapa(latitude, longitude) {
+    var map = L.map('map').setView([latitude, longitude], 14); // Alteração do valor do zoom para 15
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup('Você está aqui.')
+        .openPopup();
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        document.querySelector('p').innerHTML = "Latitude = " + position.coords.latitude + "<br>Longitude = " + position.coords.longitude;
+        renderizarMapa(position.coords.latitude, position.coords.longitude);
+    });
+} else {
+    document.querySelector('p').innerHTML = "Geolocalização não suportada pelo seu navegador.";
+}
